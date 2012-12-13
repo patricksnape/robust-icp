345673bc7d90a18267548a121d1bb90df02c6986
patricksnape <pts08@ic.ac.uk>
2012-12-13 10:38:24 +0000

    Formatting

--- lsqnormest.m
+++ lsqnormest.m
@@ -8,32 +8,26 @@
 % vectors with points. k is a scalar indicating how many neighbors the
 % normal estimation is based upon.
 %
-% Note that for large point sets, the function performs significantly
-% faster if Statistics Toolbox >= v. 7.3 is installed.
-%
 % Jakob Wilm 2010
 
 function n = lsqnormest(p, k)
-m = size(p,2);
-n = zeros(3,m);
+m = size(p, 2);
+n = zeros(3, m);
 
-v = ver('stats');
-if str2double(v.Version) >= 7.5 
-    neighbors = transpose(knnsearch(transpose(p), transpose(p), 'k', k+1));
-else
-    neighbors = k_nearest_neighbors(p, p, k+1);
-end
+neighbors = transpose(knnsearch(transpose(p), transpose(p), 'k', k + 1));
 
 for i = 1:m
-    x = p(:,neighbors(2:end, i));
-    p_bar = 1/k * sum(x,2);
+    x = p(:, neighbors(2:end, i));
+    p_bar = (1 / k) * sum(x, 2);
     
-    P = (x - repmat(p_bar,1,k)) * transpose(x - repmat(p_bar,1,k)); %spd matrix P
-    %P = 2*cov(x);
+    % spd matrix P
+    P = (x - repmat(p_bar, 1, k)) * transpose(x - repmat(p_bar, 1, k)); 
     
-    [V,D] = eig(P);
+    [V, D] = eig(P);
     
-    [~, idx] = min(diag(D)); % choses the smallest eigenvalue
+    % choses the smallest eigenvalue
+    [~, idx] = min(diag(D)); 
     
-    n(:,i) = V(:,idx);   % returns the corresponding eigenvector    
+    % returns the corresponding eigenvector
+    n(:, i) = V(:, idx);   
 end
