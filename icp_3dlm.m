792bf40278b61c06714d96a2199435f0b539309f
Patrick Snape <patricksnape@gmail.com>
2013-03-05 17:59:31 +0000

    Attempting to calculate cosine kernel ICP

--- icp_3dlm.m
+++ icp_3dlm.m
@@ -14,7 +14,13 @@ VOLUME_SIZE = 100;
 D = pointclouddt(Model, VOLUME_SIZE);
 
 % Compute derivatives of D
-[dDdX, dDdY, dDdZ] = gradient(D);
+[dDd_x, dDd_y, dDd_z] = gradient(D);
+[dDd_xx, dDd_xy, dDd_xz] = gradient(dDd_x);
+[dDd_yx, dDd_yy, dDd_yz] = gradient(dDd_y);
+[dDd_zx, dDd_zy, dDd_zz] = gradient(dDd_z);
+dDd_yx = dDd_xy;
+dDd_yz = dDd_zy;
+dDd_xz = dDd_zx;
 
 clf
 BOX = [
@@ -54,9 +60,18 @@ run_icp3d_iter = 0;
 
 icp.Data = Data;
 icp.ModelDistanceTransform = D;
-icp.ModelDx = dDdX;
-icp.ModelDy = dDdY;
-icp.ModelDz = dDdZ;
+icp.ModelDx = dDd_x;
+icp.ModelDy = dDd_y;
+icp.ModelDz = dDd_z;
+icp.ModelDxx = dDd_xx;
+icp.ModelDxy = dDd_xy;
+icp.ModelDxz = dDd_xz;
+icp.ModelDyx = dDd_yx;
+icp.ModelDyy = dDd_yy;
+icp.ModelDyz = dDd_yz;
+icp.ModelDzx = dDd_zx;
+icp.ModelDzy = dDd_zy;
+icp.ModelDzz = dDd_zz;
 icp.handle = h;
 icp.volume_size = VOLUME_SIZE;
 icp.paramscale = [1 1 1 1 VOLUME_SIZE VOLUME_SIZE VOLUME_SIZE];
@@ -114,94 +129,59 @@ i = (Tdata(:, 1));
 j = (Tdata(:, 2));
 k = (Tdata(:, 3));
 
-%% Compute distance penalties near boundaries
-distpenalty = zeros(size(Tdata, 1), 1);
-Grad_distpenalty = zeros(size(Tdata, 1), 3);
-
-LO = 3;
-HI = icp.volume_size - 3;
-
-% i < 1 .. clip and add pythagorean penalty
-I = find(i < LO);
-if ~isempty(I)
-  distpenalty(I) = distpenalty(I) + (LO - i(I)).^2;
-  Grad_distpenalty(I, 1) = Grad_distpenalty(I, 1) + 2 * (LO - i(I));
-  i(I) = LO;
-end
-
-% j < LO
-I = find(j < LO);
-if ~isempty(I)
-  distpenalty(I) = distpenalty(I) + (LO - j(I)).^2;
-  Grad_distpenalty(I, 2) = Grad_distpenalty(I, 2) + 2 * (LO - j(I));
-  j(I) = LO;
-end
-
-% k < LO
-I = find(k < LO);
-if ~isempty(I)
-  distpenalty(I) = distpenalty(I) + (LO - k(I)).^2;
-  Grad_distpenalty(I, 3) = Grad_distpenalty(I, 3) + 2 * (LO - k(I));
-  k(I) = LO;
-end
-
-% i >= HI .. clip and add pythagorean penalty
-I = find(i >= HI);
-if ~isempty(I)
-  distpenalty(I) = distpenalty(I) + (i(I) - HI).^2;
-  Grad_distpenalty(I, 1) = Grad_distpenalty(I, 1) + 2 * (i(I) - HI);
-  i(I) = HI;
-end
-
-% j >= HI
-I = find(j >= HI);
-if ~isempty(I)
-  distpenalty(I) = distpenalty(I) + (j(I) - HI).^2;
-  Grad_distpenalty(I, 2) = Grad_distpenalty(I, 2) + 2 * (j(I) - HI);
-  j(I) = HI;
-end
-
-% k >= HI
-I = find(k >= HI);
-if ~isempty(I)
-  distpenalty(I) = distpenalty(I) + (k(I) - HI).^2;
-  Grad_distpenalty(I, 3) = Grad_distpenalty(I, 3) + 2 * (k(I) - HI);
-  k(I) = HI;
-end
-
 %% Interpolate distances and calculate m-estimate
 
-dists = interp3(icp.ModelDistanceTransform, i, j, k, 'linear');
-ddists_dx = interp3(icp.ModelDx, i, j, k, 'linear');
-ddists_dy = interp3(icp.ModelDy, i, j, k, 'linear');
-ddists_dz = interp3(icp.ModelDz, i, j, k, 'linear');
+dists = interp3(icp.ModelDistanceTransform, i, j, k) / icp.volume_size;
+ddists_dx = interp3(icp.ModelDx, i, j, k) / icp.volume_size;
+ddists_dy = interp3(icp.ModelDy, i, j, k) / icp.volume_size;
+ddists_dz = interp3(icp.ModelDz, i, j, k) / icp.volume_size;
 
-% Add penalties to distances, so points outside the DT get
-% an upper-bound distance to the model.
-dists = dists + 1e-4;
-pdists = sqrt(awf_m_estimator('huber', dists, icp.volume_size) + distpenalty);
-mprime = awf_m_estimator('Dhuber', dists, icp.volume_size);
-c = mprime ./ (2 * pdists);
+% Calculate df(g1,x[0]) / dg1,x[0]
+ddists_dx2 = ddists_dx .^ 2;
+ddists_dy2 = ddists_dy .^ 2;
+ddists_dz2 = ddists_dz .^ 2;
 
-dpdists_dx = c .* (ddists_dx + Grad_distpenalty(:, 1));
-dpdists_dy = c .* (ddists_dy + Grad_distpenalty(:, 2));
-dpdists_dz = c .* (ddists_dz + Grad_distpenalty(:, 3));
-Grad_pdists = [dpdists_dx dpdists_dy dpdists_dz];
+ddists_denom = sqrt(ddists_dx2 + ddists_dy2 + ddists_dz2);
+ddists_denom = median_adjusted(ddists_denom);
+df_g1_denom = sqrt(ddists_dx2 + ddists_dy2 + ddists_dz2) .^ 3;
 
-% Scale back distances
-dists = pdists / icp.volume_size;
-Grad_pdists = Grad_pdists / icp.volume_size;
+% Prevent division by zero by adding the median
+% Take the median ignoring the dead voxels
+df_g1_denom = median_adjusted(df_g1_denom);
+
+dF_g1x = (ddists_dy2 + ddists_dz2) ./ df_g1_denom;
+dF_g1y = (ddists_dx2 + ddists_dz2) ./ df_g1_denom;
+dF_g1z = (ddists_dx2 + ddists_dy2) ./ df_g1_denom;
+
+ddists_dxx = interp3(icp.ModelDxx, i, j, k);
+ddists_dxy = interp3(icp.ModelDxy, i, j, k);
+ddists_dxz = interp3(icp.ModelDxz, i, j, k);
+ddists_dyx = interp3(icp.ModelDyx, i, j, k);
+ddists_dyy = interp3(icp.ModelDyy, i, j, k);
+ddists_dyz = interp3(icp.ModelDyz, i, j, k);
+ddists_dzx = interp3(icp.ModelDzx, i, j, k);
+ddists_dzy = interp3(icp.ModelDzy, i, j, k);
+ddists_dzz = interp3(icp.ModelDzz, i, j, k);
+
+% [g1,xx, g1,xy, g1,xz] * dW_dp
+N_p = length(params);
+L = ones(1, N_p);
+dg1x_dp = ddists_dxx(:, L) .* Jx + ddists_dxy(:, L) .* Jy + ddists_dxz(:, L) .* Jz;
+dg1y_dp = ddists_dyx(:, L) .* Jx + ddists_dyy(:, L) .* Jy + ddists_dyz(:, L) .* Jz;
+dg1z_dp = ddists_dzx(:, L) .* Jx + ddists_dzy(:, L) .* Jy + ddists_dzz(:, L) .* Jz;
+
+ddists_dx = ddists_dx ./ ddists_denom;
+ddists_dy = ddists_dy ./ ddists_denom;
+ddists_dz = ddists_dz ./ ddists_denom;
+dF_g1x = repmat(dF_g1x, 1, N_p);
+dF_g1y = repmat(dF_g1y, 1, N_p);
+dF_g1z = repmat(dF_g1z, 1, N_p);
+Jx = dF_g1x .* dg1x_dp;
+Jy = dF_g1y .* dg1y_dp;
+Jz = dF_g1z .* dg1z_dp;
 
 %% Scale Jacobian by distance transform
-
-% d/dp f(T(p)) = [d/dx f(x)](T(p)) d/dp T(p)
-%    1 x 7             1 x 3       3 x 7
-
-L = ones(1,7);
-% Replicate each column 7 times
-J = Grad_pdists(:, L*1) .* Jx + ...
-    Grad_pdists(:, L*2) .* Jy + ...
-    Grad_pdists(:, L*3) .* Jz;
+J = Jx + Jy + Jz;
 
 % Scale down J
 J = J .* PARAMSCALE(ones(size(J, 1), 1),:);
@@ -237,3 +217,10 @@ p7 = p(7);
 
 R = quat2rot([p1 p2 p3 p4]) / sum([p1 p2 p3 p4].^2);
 t = [p5 p6 p7]';
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function res = median_adjusted(b)
+bc    = b(:);
+bc(bc == 0) = NaN;
+m_b   = nanmedian(bc);
+
+res = b + m_b;
